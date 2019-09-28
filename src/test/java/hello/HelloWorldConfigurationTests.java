7f5fc99813bfc4b1f6c032841dbcb8677ca4c390
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2019-09-28 12:22:37 -0500

    Update HelloWorldConfigurationTests.java

--- src/test/java/hello/HelloWorldConfigurationTests.java
+++ src/test/java/hello/HelloWorldConfigurationTests.java
@@ -52,7 +52,7 @@ public class HelloWorldConfigurationTests {
         ResponseEntity<String> entity = restTemplate
                 .getForEntity("http://localhost:" + this.port + "/", String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals(entity.getBody(),"retoz");
+        assertEquals(entity.getBody(),"reto");
     }
 
 }
