905a0cbbe9a3013625f8da9863f538ba6891ed52
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2019-09-29 04:11:16 -0500

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "reto";
+        return "RetoDevOps Completado";
     }
     
     @RequestMapping("/otroendpoint")
